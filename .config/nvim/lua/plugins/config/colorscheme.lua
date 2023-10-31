2d2ea2e652dc1009c095205014f4d432ed452cbd
Lucas Quintino <lucas.quintino@pontoid.com.br>
2023-10-31 08:54:15 -0300

    feat: new colorscheme neosolarized

--- .config/nvim/lua/plugins/config/colorscheme.lua
+++ .config/nvim/lua/plugins/config/colorscheme.lua
@@ -1,66 +1,110 @@
 local _, tokyonight = pcall(require, "tokyonight")
 if not _ then
-    return
+	return
 end
 
 tokyonight.setup({
-    style = "storm",
-    light_style = "day",
-    transparent = true,
-    terminal_colors = true,
-    styles = {
-        comments = { italic = true },
-        keywords = { italic = true },
-        functions = {},
-        variables = {},
-        sidebars = "dark",
-        floats = "dark",
-    },
-    sidebars = { "qf", "help" },
-    day_brightness = 0.3,
-    hide_inactive_statusline = false,
-    dim_inactive = false,
-    lualine_bold = true,
+	style = "storm",
+	light_style = "day",
+	transparent = true,
+	terminal_colors = true,
+	styles = {
+		comments = { italic = true },
+		keywords = { italic = true },
+		functions = {},
+		variables = {},
+		sidebars = "dark",
+		floats = "dark",
+	},
+	sidebars = { "qf", "help" },
+	day_brightness = 0.3,
+	hide_inactive_statusline = false,
+	dim_inactive = false,
+	lualine_bold = true,
 })
 
 local _, dracula = pcall(require, "dracula")
 if not _ then
-    return
+	return
 end
 
 dracula.setup({
-    colors = {
-        bg = "#282A36",
-        fg = "#F8F8F2",
-        selection = "#44475A",
-        comment = "#6272A4",
-        red = "#FF5555",
-        orange = "#FFB86C",
-        yellow = "#F1FA8C",
-        green = "#50fa7b",
-        purple = "#BD93F9",
-        cyan = "#8BE9FD",
-        pink = "#FF79C6",
-        bright_red = "#FF6E6E",
-        bright_green = "#69FF94",
-        bright_yellow = "#FFFFA5",
-        bright_blue = "#D6ACFF",
-        bright_magenta = "#FF92DF",
-        bright_cyan = "#A4FFFF",
-        bright_white = "#FFFFFF",
-        menu = "#21222C",
-        visual = "#3E4452",
-        gutter_fg = "#4B5263",
-        nontext = "#3B4048",
-        white = "#ABB2BF",
-        black = "#191A21",
-    },
-    show_end_of_buffer = true,
-    transparent_bg = true,
-    lualine_bg_color = "#44475a",
-    italic_comment = true,
-    overrides = {
-        NeoTreeNormal = { bg = "#282A36" },
-        NeoTreeNormalNC = { bg = "#282A36" },
-    },
+	colors = {
+		bg = "#282A36",
+		fg = "#F8F8F2",
+		selection = "#44475A",
+		comment = "#6272A4",
+		red = "#FF5555",
+		orange = "#FFB86C",
+		yellow = "#F1FA8C",
+		green = "#50fa7b",
+		purple = "#BD93F9",
+		cyan = "#8BE9FD",
+		pink = "#FF79C6",
+		bright_red = "#FF6E6E",
+		bright_green = "#69FF94",
+		bright_yellow = "#FFFFA5",
+		bright_blue = "#D6ACFF",
+		bright_magenta = "#FF92DF",
+		bright_cyan = "#A4FFFF",
+		bright_white = "#FFFFFF",
+		menu = "#21222C",
+		visual = "#3E4452",
+		gutter_fg = "#4B5263",
+		nontext = "#3B4048",
+		white = "#ABB2BF",
+		black = "#191A21",
+	},
+	show_end_of_buffer = true,
+	transparent_bg = true,
+	lualine_bg_color = "#44475a",
+	italic_comment = true,
+	overrides = {
+		NeoTreeNormal = { bg = "#282A36" },
+		NeoTreeNormalNC = { bg = "#282A36" },
+	},
+})
+
+local _, solarized = pcall(require, "solarized")
+if not _ then
+	return
+end
+
+solarized.setup({
+	transparent = false,
+	styles = {
+		comments = {},
+		functions = {},
+		variables = {},
+		numbers = {},
+		constants = {},
+		parameters = {},
+		keywords = {},
+		types = {},
+	},
+	enables = {
+		bufferline = true,
+		cmp = true,
+		diagnostic = true,
+		dashboard = true,
+		editor = true,
+		gitsign = true,
+		hop = true,
+		indentblankline = true,
+		lsp = true,
+		lspsaga = true,
+		navic = true,
+		neotree = true,
+		notify = true,
+		semantic = true,
+		syntax = true,
+		telescope = true,
+		tree = true,
+		treesitter = true,
+		whichkey = true,
+		mini = true,
+	},
+	highlights = {},
+	colors = {},
+	theme = "neo",
 })
--- .config/nvim/lua/plugins/lazy.lua
+++ .config/nvim/lua/plugins/lazy.lua
@@ -24,6 +24,7 @@ local plugins = {
 	-- Colorscheme
 	{ "folke/tokyonight.nvim" },
 	{ "Mofiqul/dracula.nvim" },
+	{ "maxmx03/solarized.nvim" },
 
 	-- Syntax
 	{ "nvim-treesitter/nvim-treesitter" },
