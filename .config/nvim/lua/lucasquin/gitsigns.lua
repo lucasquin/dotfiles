57e8a29306651f73ed9f1174ab08bcea808aaef0
lucasquin <lucaslquintino@gmail.com>
2024-01-13 17:37:04 -0300

    gitsigns fix

--- .config/nvim/lua/lucasquin/gitsigns.lua
+++ .config/nvim/lua/lucasquin/gitsigns.lua
@@ -1,18 +1,49 @@
 local M = {
   "lewis6991/gitsigns.nvim",
   enabled = vim.fn.executable "git" == 1,
-  ft = "gitcommit",
 }
 
 M.config = function()
   require("gitsigns").setup {
     signs = {
-      add = { text = "▎" },
-      change = { text = "▎" },
-      delete = { text = "▎" },
+      add = { text = "│" },
+      change = { text = "│" },
+      delete = { text = "_" },
       topdelete = { text = "契" },
-      changedelete = { text = "▎" },
-      untracked = { text = "▎" },
+      changedelete = { text = "~" },
+      untracked = { text = "┆" },
+    },
+    signcolumn = true,
+    numhl = false,
+    linehl = false,
+    word_diff = false,
+    watch_gitdir = {
+      follow_files = true,
+    },
+    auto_attach = true,
+    attach_to_untracked = true,
+    current_line_blame = false,
+    current_line_blame_opts = {
+      virt_text = true,
+      virt_text_pos = "eol", -- 'eol' | 'overlay' | 'right_align'
+      delay = 1000,
+      ignore_whitespace = false,
+      virt_text_priority = 100,
+    },
+    current_line_blame_formatter = "<author>, <author_time:%Y-%m-%d> - <summary>",
+    sign_priority = 6,
+    update_debounce = 100,
+    status_formatter = nil,
+    max_file_length = 40000,
+    preview_config = {
+      border = "single",
+      style = "minimal",
+      relative = "cursor",
+      row = 0,
+      col = 1,
+    },
+    yadm = {
+      enable = false,
     },
   }
 end
