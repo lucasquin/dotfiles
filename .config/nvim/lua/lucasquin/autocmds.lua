bc96de68ff813da77c6fa7359ed96138b121d3ce
lucasquin <lucaslquintino@gmail.com>
2024-02-11 16:42:02 -0300

    fix: svelte lint

--- .config/nvim/lua/lucasquin/autocmds.lua
+++ .config/nvim/lua/lucasquin/autocmds.lua
@@ -5,3 +5,16 @@ vim.cmd [[
     autocmd BufLeave,FocusLost,InsertEnter,WinLeave   * if &nu                  | set nornu | endif
     augroup END
   ]]
+
+function disable_diagnostic()
+  local file = vim.bo.filetype
+  return file == "svelte" or file == "neo-tree"
+end
+
+vim.cmd [[
+    augroup diagnostics
+    autocmd!
+    autocmd BufEnter * lua if disable_diagnostic() then vim.diagnostic.disable() end
+    autocmd BufEnter * lua if not disable_diagnostic() then vim.diagnostic.enable() end
+    augroup END
+  ]]
--- .config/nvim/lua/lucasquin/lsp.lua
+++ .config/nvim/lua/lucasquin/lsp.lua
@@ -37,6 +37,7 @@ M.config = function()
     },
     handlers = {
       lsp_zero.default_setup,
+
       lua_ls = function()
         local lua_opts = lsp_zero.nvim_lua_ls()
         require("lspconfig").lua_ls.setup(lua_opts)
@@ -44,6 +45,7 @@ M.config = function()
     },
     automatic_installation = true,
   }
+
 end
 
 return M
